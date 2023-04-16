3fc0203769c285201adac6bb483fae19b9b912ba
Harry <44312007+harryber@users.noreply.github.com>
2023-04-15 17:26:22 -0700

    test update

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -47,7 +47,7 @@ public class AIController : MonoBehaviour
     void Start()
     {
         // API KEY GOES HERE
-        api = new OpenAIAPI("");
+        api = new OpenAIAPI("a");
         messages = new List<ChatMessage>
         {
             new ChatMessage(ChatMessageRole.System, "You are about to start generating educational questions for users to solve.")
